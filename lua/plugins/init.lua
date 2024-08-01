469119540b0897ceb81455c287d23b4bbead33e6
evhman <165422817+evhman@users.noreply.github.com>
2024-08-01 16:00:54 +0200

    coq is shit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -67,29 +67,6 @@ return {
 
   {
     "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
-    lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
-    dependencies = {
-      -- main one
-      { "ms-jpq/coq_nvim", branch = "coq" },
-
-      -- 9000+ Snippets
-      { "ms-jpq/coq.artifacts", branch = "artifacts" },
-
-      -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-      -- Need to **configure separately**
-      { 'ms-jpq/coq.thirdparty', branch = "3p" }
-      -- - shell repl
-      -- - nvim lua api
-      -- - scientific calculator
-      -- - comment banner
-      -- - etc
-    },
-    init = function()
-      vim.g.coq_settings = {
-          auto_start = true, -- if you want to start COQ at startup
-          -- Your COQ settings here
-      }
-    end,
     config = function()
       require "configs.lspconfig"
     end,
