7375573665baef90788a3dfd35e95bab01e2def3
thomas <thomas@HomePC.home>
2024-10-20 18:33:17 +0200

    Add support for bashls
    Signed-off-by: thomas <thomas@HomePC.home>

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "clangd", "pyright" }
+local servers = { "clangd", "pyright", "bashls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
